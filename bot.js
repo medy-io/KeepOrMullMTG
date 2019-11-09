e0828283ad52311cbdcd87be4dfbff8927a2eca0
crsmyz <crsmyz@gmail.com>
2019-11-09 10:44:41 -0500

    removing secrets

--- bot.js
+++ bot.js
@@ -2,10 +2,10 @@
 var Twit = require('twit'),
     Twitter = new Twit(
         {
-            consumer_key: '9amG0HeJpVxYm5PCSzQM2Kd98', //process.env.BOT_CONSUMER_KEY,
-            consumer_secret: 'PHSiSWPQJ6kfrj8npKiquWlBinOO6yH8IIaSYWG4wrXLI3pvbV',// process.env.BOT_CONSUMER_SECRET,
-            access_token: '1004735994552299520-zOn0vy1cPxylLeWRNyal0tliVcxUR5',// process.env.BOT_ACCESS_TOKEN,
-            access_token_secret: 'cPwS5ZGw3JX8G6uYlISwgvxLbuQncqHbqMe9nWmvo1Rhe', // process.env.BOT_ACCESS_TOKEN_SECRET
+            consumer_key: process.env.BOT_CONSUMER_KEY,
+            consumer_secret: process.env.BOT_CONSUMER_SECRET,
+            access_token: process.env.BOT_ACCESS_TOKEN,
+            access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
         }
     ),
     request = require('request'),
